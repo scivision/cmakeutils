76c83db4348211bbc717193c15639e4bb320d20d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-05 15:40:40 -0500

    require libssl

--- cmake_setup.sh
+++ cmake_setup.sh
@@ -16,7 +16,11 @@ cver=3.13.1
 PREF=$HOME/.local
 WD=/tmp
 
-set -e # after prereqs
+set -e
+
+# 0. check prereqs
+[[ $(ldconfig -p | grep ssl) ]] || { echo "must have SSL development library installed"; exit 1; }
+
 
 # 1. download
 [[ -d $WD/cmake-$cver ]] || curl https://cmake.org/files/v${cver:0:4}/cmake-$cver.tar.gz | tar -C $WD  -xzf -
@@ -26,9 +30,13 @@ set -e # after prereqs
 cd $WD
 
 echo "installing cmake to $PREF"
+
 ./cmake-$cver/bootstrap --prefix=$PREF --parallel=2 -- -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_USE_OPENSSL:BOOL=ON
 
 make -j -l 2
+
+mkdir -p $PREF
+
 make install
 )
 
