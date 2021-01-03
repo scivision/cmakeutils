1ee4697395f00247f74184fd8ef140dc50ce8674
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-02 23:39:54 -0500

    libtool not needed

--- cmake/Modules/FindAutotools.cmake
+++ cmake/Modules/FindAutotools.cmake
@@ -29,11 +29,7 @@ NAMES gmake make
 NAMES_PER_DIR
 DOC "GNU Make")
 
-find_program(LIBTOOL_EXECUTABLE
-NAMES libtoolize
-DOC "libtool sets up shared libraries")
-
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Autotools
-  REQUIRED_VARS AUTOCONF_EXECUTABLE AUTOMAKE_EXECUTABLE MAKE_EXECUTABLE LIBTOOL_EXECUTABLE)
+  REQUIRED_VARS AUTOCONF_EXECUTABLE AUTOMAKE_EXECUTABLE MAKE_EXECUTABLE)
