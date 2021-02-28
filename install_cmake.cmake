d234fd19c8b81a18b8fe76e94ba2e8d19af08a26
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-28 10:17:29 -0500

    cmake 3.19.6

--- install_cmake.cmake
+++ install_cmake.cmake
@@ -11,7 +11,7 @@
 # This script can be used to install CMake >= 2.8.12.2 (e.g. for compatibility tests)
 # old CMake versions have broken file(DOWNLOAD)--they just "download" 0-byte files.
 
-cmake_minimum_required(VERSION 3.7)
+cmake_minimum_required(VERSION 3.7...3.20)
 
 if(NOT prefix)
   if(WIN32)
@@ -22,7 +22,7 @@ if(NOT prefix)
 endif()
 
 if(NOT version)
-  set(version 3.19.4)
+  set(version 3.19.6)
 endif()
 
 if(version STREQUAL 2.8.12)
