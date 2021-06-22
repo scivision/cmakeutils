f42fc8c6c83a54614a24fcb612e534fc59f02c57
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-22 13:37:17 -0400

    cmake 3.20.5, zstd 1.5.0

--- build_zstd.cmake
+++ build_zstd.cmake
@@ -1,24 +1,33 @@
-cmake_minimum_required(VERSION 3.19)
+cmake_minimum_required(VERSION 3.19...3.20)
 
-get_filename_component(CMAKE_INSTALL_PREFIX ~/zstd ABSOLUTE)
+set(version 1.5.0)
 
-if(DEFINED ENV{TMP})
-  set(tmp $ENV{TMP})
-elseif(DEFINED ENV{TMPDIR})
-  set(tmp $ENV{TMPDIR})
+get_filename_component(CMAKE_INSTALL_PREFIX ~/zstd-${version} ABSOLUTE)
+
+set(CMAKE_TLS_VERIFY true)
+
+if(DEFINED ENV{TMPDIR})
+  set(tmpdir $ENV{TMPDIR})
+elseif(IS_DIRECTORY /var/tmp)
+  set(tmpdir /var/tmp)
+elseif(IS_DIRECTORY /tmp)
+  set(tmpdir /tmp)
 else()
-  set(tmp ~/tmp)
+  set(tmpdir ~/tmp)
 endif()
 
-get_filename_component(tmp ${tmp} ABSOLUTE)
+get_filename_component(tmpdir ${tmpdir} ABSOLUTE)
 
-set(src ${tmp}/zstd/build/cmake)
+set(name zstd-${version}.tar.gz)
+set(archive ${tmpdir}/${name})
+
+set(src ${tmpdir}/zstd-${version}/build/cmake)
 set(build ${src}/build)
 
 if(NOT IS_DIRECTORY ${src})
-  find_package(Git REQUIRED)
-  execute_process(COMMAND ${GIT_EXECUTABLE} -C ${tmp} clone https://github.com/facebook/zstd/
-  COMMAND_ERROR_IS_FATAL ANY)
+  file(DOWNLOAD https://github.com/facebook/zstd/releases/download/v${version}/${name} ${archive}
+    INACTIVITY_TIMEOUT 15)
+  file(ARCHIVE_EXTRACT INPUT ${archive} DESTINATION ${tmpdir})
 endif()
 
 execute_process(COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX} -S ${src} -B ${build}
