e6ebd2f3cfa860000bfac89579fced3e18fb63d8
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-14 15:11:09 -0500

    use quotes for possible undefined

--- syntax/var_env_cache.cmake
+++ syntax/var_env_cache.cmake
@@ -2,6 +2,9 @@ cmake_minimum_required(VERSION 3.12...3.23)
 # CMake if() is NOT short circuit.
 # this requires care with undefined variables
 
+# Special variables like environment and cache are not evaulated except with $ prefix
+# this is distinct from "normal" CMake variables.
+
 set(ENV{myvar123} true)
 if(ENV{myvar123})
   message(FATAL_ERROR "unexpected behavior with env variable not evaluated .")
@@ -11,6 +14,11 @@ if($ENV{myvar123})
 endif()
 
 set(ENV{myvar123} false)
-if($ENV{myvar123})
+if("$ENV{myvar123}")
   message(FATAL_ERROR "unexpected behavior with env variable evaluated as string.")
+endif()
+
+set(ENV{myvar123})
+if(0 OR "$ENV{myvar123}")
+  message(FATAL_ERROR "unexpected behavior with env variable")
 endif()
