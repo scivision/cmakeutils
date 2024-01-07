145d0744cdf0871f672053554cab00cae6e63d74
scivision <scivision@users.noreply.github.com>
2024-01-06 23:43:34 -0500

    print log on fail

--- bad_ssl.cmake
+++ bad_ssl.cmake
@@ -10,26 +10,46 @@ string(JSON ua GET ${meta} 0 ua rawUa)
 message(STATUS "CMake ${CMAKE_VERSION}
 cURL version: ${ua}
 TLS_CAINFO: ${CMAKE_TLS_CAINFO}
+SSL_CERT_DIR: $ENV{SSL_CERT_DIR}
 SSL_CERT_FILE: $ENV{SSL_CERT_FILE}"
 )
 
 
-function(ssl url)
+function(ssl url retval)
 
-file(DOWNLOAD ${url} STATUS ret)
+file(DOWNLOAD ${url} STATUS ret LOG log)
 
 list(GET ret 0 status)
 list(GET ret 1 msg)
 
 message(STATUS "${url}:${status}: ${msg}")
 
+if(NOT status EQUAL 0)
+  message(NOTICE "${log}")
+endif()
+
+set(${retval} ${status} PARENT_SCOPE)
+
 endfunction(ssl)
 
 
 message(STATUS "These tests should fail.")
-ssl(https://expired.badssl.com/)
-ssl(https://untrusted-root.badssl.com/)
+
+foreach(url IN ITEMS https://expired.badssl.com/ https://untrusted-root.badssl.com/)
+  ssl(${url} status)
+  if(status EQUAL 0)
+    message(SEND_ERROR "FAIL: ${url}")
+  else()
+    message(STATUS "OK: ${url}")
+  endif()
+endforeach()
 
 message(STATUS "These tests should work OK.")
-ssl(https://hsts.badssl.com/)
-ssl(https://sha256.badssl.com/)
+foreach(url IN ITEMS https://hsts.badssl.com/ https://sha256.badssl.com/)
+  ssl(${url} status)
+  if(status EQUAL 0)
+    message(STATUS "OK: ${url}")
+  else()
+    message(SEND_ERROR "FAIL: ${url}")
+  endif()
+endforeach()
