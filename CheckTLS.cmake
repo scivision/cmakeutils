b46e4edd50772840990c9bbadcec8721f66303da
scivision <10931741+scivision@users.noreply.github.com>
2024-01-07 00:08:10 -0500

    Update CheckTLS.cmake
    from Gist

--- CheckTLS.cmake
+++ CheckTLS.cmake
@@ -1,27 +1,25 @@
-cmake_minimum_required(VERSION 3.20)
+cmake_minimum_required(VERSION 3.19)
 
 function(check_tls)
 # some CMake may not have SSL/TLS enabled, or may have missing/broken system certificates.
 # this is a publicly-usable service (as per their TOS)
 
 set(url https://www.howsmyssl.com/a/check)
-cmake_path(SET tls_check_file ${PROJECT_BINARY_DIR}/check_tls.json)
+set(temp ${PROJECT_BINARY_DIR}/check_tls.json)
 
-if(EXISTS ${tls_check_file})
-  # we've already checked this computer's TLS configuration.
-  return()
-endif()
+message(STATUS "CheckTLS: ${url} => ${temp}")
+file(DOWNLOAD ${url} ${temp} INACTIVITY_TIMEOUT 5 TLS_VERIFY on)
 
-message(STATUS "CheckTLS: ${url} => ${tls_check_file}")
-file(DOWNLOAD ${url} ${tls_check_file} INACTIVITY_TIMEOUT 10)
-
-file(READ ${tls_check_file} json)
-
+file(READ ${temp} json)
 string(JSON rating ERROR_VARIABLE e GET ${json} rating)
 
-message(STATUS "TLS status: ${rating}")
+message(STATUS "CMake ${CMAKE_VERSION} TLS status: ${rating}")
 if(NOT rating STREQUAL "Probably Okay")
-  message(WARNING "TLS seems to be broken on your system. Download will probably fail.  ${rating}")
+  message(WARNING "TLS seems to be broken. Download will probably fail.  ${rating}")
 endif()
 
 endfunction(check_tls)
+
+if(CMAKE_SCRIPT_MODE_FILE)
+  check_tls()
+endif()
