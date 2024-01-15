71fc896b299a480b4443a8ef2ad8cbb88d0ee64d
scivision <scivision@users.noreply.github.com>
2024-01-15 14:23:58 -0500

    add regex and list(TRANSFORM) examples

---
+++ regex/transform.cmake
@@ -0,0 +1,36 @@
+cmake_minimum_required(VERSION 3.12)
+
+# examples of list(TRANSFORM ...)
+# these examples come from CMake self-test and various GitLab Issues and Discourse.
+
+message(STATUS "Extract major.minor version from a version string")
+
+set(x 1.2.3.4 1.2.3 1.2 1)
+message(STATUS "before: ${x}")
+list(TRANSFORM x REPLACE "^([0-9]+\\.[0-9]+).*" "\\1")
+message(STATUS "after: ${x}")
+
+# SELECTOR examples
+
+set(in alpha bravo charlie delta)
+
+list(TRANSFORM in REPLACE "(.+a)$" "\\1_\\1" OUTPUT_VARIABLE out)
+message(STATUS "${out}")
+
+list(TRANSFORM in REPLACE "(.+a)$" "\\1_\\1" AT 1 3 OUTPUT_VARIABLE out)
+message(STATUS "AT 1 3    ${out}")
+
+list(TRANSFORM in REPLACE "(.+e)$" "\\1_\\1" AT 1 -2 OUTPUT_VARIABLE out)
+message(STATUS "AT 1 -2   ${out}")
+
+list(TRANSFORM in REPLACE "(.+e)$" "\\1_\\1" FOR 1 2 OUTPUT_VARIABLE out)
+message(STATUS "FOR 1 2   ${out}")
+
+list(TRANSFORM in REPLACE "(.+a)$" "\\1_\\1" FOR 1 -1 OUTPUT_VARIABLE out)
+message(STATUS "FOR 1 -1  ${out}")
+
+list(TRANSFORM in REPLACE "(.+a)$" "\\1_\\1" FOR 0 -1 2 OUTPUT_VARIABLE out)
+message(STATUS "FOR 0 -1 2 ${out}")
+
+list(TRANSFORM in REPLACE "(.+a)$" "\\1_\\1" REGEX "(r|t)a" OUTPUT_VARIABLE out)
+message(STATUS "REGEX (r|t)a   ${out}")
