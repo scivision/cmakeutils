2ceb116618f93eb684a7d54ff91edc1093a5df86
scivision <scivision@users.noreply.github.com>
2024-03-01 00:15:00 -0500

    improve tlsverify.cmake [skip ci]

--- TLSversion.cmake
+++ TLSversion.cmake
@@ -1,28 +1,40 @@
 cmake_minimum_required(VERSION 3.29)  # actually 3.30 when out of nightly
 
-foreach(url IN ITEMS https://tls-v1-0.badssl.com:1010/ https://tls-v1-1.badssl.com:1011/ https://tls-v1-2.badssl.com:1012/)
+message(STATUS "CMake version: ${CMAKE_VERSION}")
 
-file(DOWNLOAD ${url}
-TLS_VERIFY on
-TLS_VERSION 1.3
-LOG log STATUS stat
-)
-list(GET stat 0 code)
-if(code EQUAL 0)
-  message(SEND_ERROR "${url} should fail")
-endif()
-message(STATUS "${url} status: ${stat}")
-message(STATUS "${url} log: ${log}")
+function(dl url tv)
 
+file(DOWNLOAD ${url} TLS_VERIFY on TLS_VERSION ${tv} LOG log STATUS stat)
+
+message(STATUS "${url} TLS v${tv} status: ${stat}")
+message(VERBOSE "${url} TLS v${tv} log: ${log}")
+
+endfunction(dl)
+
+
+set(urls https://hsts.badssl.com https://tls-v1-2.badssl.com:1012/ https://tls-v1-1.badssl.com:1011/ https://tls-v1-0.badssl.com:1010/)
+# badssl.com didn't yet have TLS v1.3
+
+foreach(tv IN ITEMS 1.0 1.1 1.2 1.3)
+    foreach(url IN LISTS urls)
+        dl(${url} ${tv})
+    endforeach()
 endforeach()
 
-
-set(url https://hsts.badssl.com)
-# badssl.com didn't yet have TLS v1.3
-file(DOWNLOAD ${url} TLS_VERIFY on TLS_VERSION 1.2 LOG log STATUS stat)
-list(GET stat 0 code)
-if(NOT code EQUAL 0)
-  message(SEND_ERROR "${url} should succeed")
-endif()
-message(STATUS "${url} status: ${stat}")
-message(STATUS "${url} log: ${log}")
+# ~\cmake-20240228\bin/cmake -P .\TLSversion.cmake
+# -- https://hsts.badssl.com TLS v1.0 status: 0;"No error"
+# -- https://tls-v1-2.badssl.com:1012/ TLS v1.0 status: 0;"No error"
+# -- https://tls-v1-1.badssl.com:1011/ TLS v1.0 status: 0;"No error"
+# -- https://tls-v1-0.badssl.com:1010/ TLS v1.0 status: 0;"No error"
+# -- https://hsts.badssl.com TLS v1.1 status: 0;"No error"
+# -- https://tls-v1-2.badssl.com:1012/ TLS v1.1 status: 0;"No error"
+# -- https://tls-v1-1.badssl.com:1011/ TLS v1.1 status: 0;"No error"
+# -- https://tls-v1-0.badssl.com:1010/ TLS v1.1 status: 35;"SSL connect error"
+# -- https://hsts.badssl.com TLS v1.2 status: 0;"No error"
+# -- https://tls-v1-2.badssl.com:1012/ TLS v1.2 status: 0;"No error"
+# -- https://tls-v1-1.badssl.com:1011/ TLS v1.2 status: 35;"SSL connect error"
+# -- https://tls-v1-0.badssl.com:1010/ TLS v1.2 status: 35;"SSL connect error"
+# -- https://hsts.badssl.com TLS v1.3 status: 35;"SSL connect error"
+# -- https://tls-v1-2.badssl.com:1012/ TLS v1.3 status: 35;"SSL connect error"
+# -- https://tls-v1-1.badssl.com:1011/ TLS v1.3 status: 35;"SSL connect error"
+# -- https://tls-v1-0.badssl.com:1010/ TLS v1.3 status: 35;"SSL connect error"
