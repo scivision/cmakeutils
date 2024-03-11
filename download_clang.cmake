9c50e1880c8f42b13db5dc03bbf32103cf250ff2
scivision <scivision@users.noreply.github.com>
2024-03-11 18:25:47 -0400

    download clang [skip ci]

---
+++ download_clang.cmake
@@ -0,0 +1,56 @@
+# downloads and extracts Clang LLVM binaries if available
+# not a robust script at all
+
+cmake_minimum_required(VERSION 3.25...3.29)
+
+option(CMAKE_TLS_VERIFY "TLS verification" ON)
+
+include(FetchContent)
+
+message(STATUS "CMake ${CMAKE_VERSION}")
+
+set(head clang+llvm)
+
+set(version 18.1.1)
+
+if(NOT prefix)
+  set(prefix ~/clang-${version})
+endif()
+
+get_filename_component(prefix ${prefix} ABSOLUTE)
+
+if(NOT DEFINED n)
+if(WIN32)
+  set(n x86_64-pc-windows-msvc)
+else()
+  execute_process(COMMAND uname -m
+  OUTPUT_VARIABLE unamem OUTPUT_STRIP_TRAILING_WHITESPACE
+  COMMAND_ERROR_IS_FATAL ANY
+  )
+  execute_process(COMMAND uname -s
+  OUTPUT_VARIABLE unames OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if(unamem MATCHES "poweerpc")
+    if(unames MATCHES "AIX")
+      set(n powerpc64-ibm-aix-7.2)
+    elseif(LINUX)
+      set(n powerpc64le-linux-rhel-8.8)
+    endif()
+  endif()
+endif()
+endif()
+
+if(NOT DEFINED n)
+  message(FATAL_ERROR "unsupported platform")
+endif()
+
+set(tail .tar.xz)
+
+set(url "https://github.com/llvm/llvm-project/releases/download/llvmorg-${version}/${head}-${version}-${n}${tail}")
+
+message(STATUS "${url} => ${prefix}")
+
+FetchContent_Populate(llvm
+URL ${url}
+SOURCE_DIR ${prefix}
+)
