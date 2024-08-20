c39224c7a804bce92a14d5b7825870e32d5d7d5f
scivision <scivision@users.noreply.github.com>
2024-08-20 16:17:00 -0400

    MPICH: pass options correctly

--- scripts/build_mpich.cmake
+++ scripts/build_mpich.cmake
@@ -2,8 +2,22 @@
 # cmake -Dprefix=~/mpi -P build_mpich.cmake
 cmake_minimum_required(VERSION 3.20)
 
+set(args -Dmpich:BOOL=true)
+
+if(prefix)
+  list(APPEND args -Dprefix=${prefix})
+endif()
+
+if(mpi_url)
+  list(APPEND args -Dmpi_url=${mpi_url})
+endif()
+
+if(bindir)
+  list(APPEND args -Dbindir=${bindir})
+endif()
+
 execute_process(COMMAND
-  ${CMAKE_COMMAND} -Dargs=-Dmpich:BOOL=true
+  ${CMAKE_COMMAND} -Dargs=${args}
   -P ${CMAKE_CURRENT_LIST_DIR}/build_openmpi.cmake
 )
 
