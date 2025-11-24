227ad902dc6e966d8117d97586a89ae3e985b756
scivision <scivision@users.noreply.github.com>
2025-11-23 20:35:24 -0500

    print status if fail

--- UserAgent.cmake
+++ UserAgent.cmake
@@ -5,11 +5,15 @@ function(user_agent)
 set(url https://www.whatsmyua.info/api/v1/ua)
 set(file ${CMAKE_CURRENT_BINARY_DIR}/ua.json)
 
-file(DOWNLOAD ${url} ${file})
+file(DOWNLOAD ${url} ${file} STATUS s)
+list(GET s 0 ret)
+if(NOT ret EQUAL 0)
+  message(FATAL_ERROR "failed to get UserAgent from ${url}. ${s}")
+endif()
 # CMake UserAgent like curl/7.69.0
 file(READ ${file} json)
 
-string(JSON ua GET ${json} 0 "ua" "rawUa")
+string(JSON ua GET "${json}" 0 "ua" "rawUa")
 
 message(STATUS "User agent: ${ua}")
 if(DEFINED ENV{SSL_CERT_FILE})
